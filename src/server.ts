d1490afedb0ed59197ec79ab75a03d6ddf5dd44a
Shafkatahmad <shafkataahmad@gmail.com>
2026-05-10 22:51:44 +0600

    organized file

--- src/server.ts
+++ src/server.ts
@@ -1,23 +1,8 @@
 import { createServer, IncomingMessage, Server } from "http";
+import { routeHandler } from "./routers/router";
 
 const server: Server = createServer((req: IncomingMessage, res) => {
-  // console.log(req.url);     // '/', '/user', 'products'
-  // console.log(req.method);  // "GET", "POST", "DELETE"
-
-  const url = req.url;
-  const method = req.method;
-
-  if (url === "/" && method === "GET") {
-    // console.log("This is root route");
-    res.writeHead(200, { "content-type": "application/json" });
-    res.end(JSON.stringify({ message: "This is root route" }));
-  } else if (url?.startsWith("/product")) {
-    res.writeHead(200, { "content-type": "application/json" });
-    res.end(JSON.stringify({ message: "This is products route" }));
-  } else {
-    res.writeHead(404, { "content-type": "application/json" });
-    res.end(JSON.stringify({ message: "Route not found" }));
-  }
+  routeHandler(req, res);
 });
 
 server.listen(5000, () => {
